3ad0f8f4d791d7d5caddd7b2723033835abdc042
Chandoeurn Chorm <chandoeurn708@gmail.com>
2026-06-13 08:57:50 +0700

    back slas

--- src/views/cart/CartView.vue
+++ src/views/cart/CartView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="w-[90%] mx-auto py-8">
+    <div class="p-2 w-20 mt-2  ">
+        <RouterLink class="border  rounded-2xl justify-center flex items-center hover:bg-amber-400 hover:border hover:text-white hover:scale-95 hover:border-amber-600 duration-500 text-black text-sm" to="/">
+            <span><i class="fa-solid fa-chevron-left"></i></span>
+            Back
+        </RouterLink>       
+    </div>
+   <div class="w-[90%] mx-auto py-8">
     <h1 class="text-3xl font-bold mb-6">
       Shopping Cart
     </h1>
--- src/views/order/OrderTrackingView.vue
+++ src/views/order/OrderTrackingView.vue
@@ -1,4 +1,10 @@
 <template>
+  <div class="p-2 w-20 mt-2  ">
+        <RouterLink class="border  rounded-2xl justify-center flex items-center hover:bg-amber-400 hover:border hover:text-white hover:scale-95 hover:border-amber-600 duration-500 text-black text-sm" to="/">
+            <span><i class="fa-solid fa-angles-left"></i></span>
+            Back
+        </RouterLink>       
+    </div>
   <div>
     <h1 class="text-3xl font-bold">
       Order Tracking
